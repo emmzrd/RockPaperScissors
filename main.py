f7dd4a21d577de124fc734bcbe47c60bcdec2bcf
emmzrd <61377267+emmzrd@users.noreply.github.com>
2022-02-19 13:30:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+
 rock = '''
     _______
 ---'   ____)
@@ -27,57 +28,56 @@ scissors = '''
 '''
 
 print("Let's play Rock, Paper, Scissors!")
-player=input("What do you choose?\nType 0 for Rock, 1 for Paper or 2 for Scissors.\n")
+player = input("What do you choose?\nType 0 for Rock, 1 for Paper or 2 for Scissors.\n")
 
-computer_random=random.randint(0,2)
-#ROCK
-if player=="0":
-  print("Player chose Rock")
-  print(rock)
-  if computer_random==0:
-    print("Computer chose Rock")
+computer_random = random.randint(0, 2)
+# ROCK
+if player == "0":
+    print("Player chose Rock")
     print(rock)
-    print("It's a tie")
-  elif computer_random==1:
-    print("Computer chose Paper")
+    if computer_random == 0:
+        print("Computer chose Rock")
+        print(rock)
+        print("It's a tie")
+    elif computer_random == 1:
+        print("Computer chose Paper")
+        print(paper)
+        print("You Lose!")
+    elif computer_random == 2:
+        print("Computer chose Scissors")
+        print(scissors)
+        print("You Win!")
+# PAPER
+elif player == "1":
+    print("Player chose Paper")
     print(paper)
-    print("You Lose!")
-  elif computer_random==2:
-      print("Computer chose Scissors")
-      print(scissors)
-      print("You Win!")
-#PAPER
-elif player=="1":
-  print("Player chose Paper")
-  print(paper)
-  if computer_random==0:
-    print("Computer chose Rock")
-    print(rock)
-    print("You Win!")
-  elif computer_random==1:
-    print("Computer chose Paper")
-    print(paper)
-    print("It's a Tie!")
-  elif computer_random==2:
-    print("Computer chose Scissors")
+    if computer_random == 0:
+        print("Computer chose Rock")
+        print(rock)
+        print("You Win!")
+    elif computer_random == 1:
+        print("Computer chose Paper")
+        print(paper)
+        print("It's a Tie!")
+    elif computer_random == 2:
+        print("Computer chose Scissors")
+        print(scissors)
+        print("You Lose!")
+# SCISSORS
+elif player == "2":
+    print("Player chose Scissors")
     print(scissors)
-    print("You Lose!")
-#SCISSORS
-elif player=="2":
-  print("Player chose Scissors")
-  print(scissors)
-  if computer_random==0:
-    print("Computer chose Rock")
-    print(f"{rock}")
-    print("You Lose!")
-  elif computer_random==1:
-    print("Computer chose Paper")
-    print(paper)
-    print("You Win!")
-  elif computer_random==2:
-    print("Computer chose Scissors")
-    print(scissors)
-    print("It's a Tie!")
+    if computer_random == 0:
+        print("Computer chose Rock")
+        print(f"{rock}")
+        print("You Lose!")
+    elif computer_random == 1:
+        print("Computer chose Paper")
+        print(paper)
+        print("You Win!")
+    elif computer_random == 2:
+        print("Computer chose Scissors")
+        print(scissors)
+        print("It's a Tie!")
 else:
-  print("Incorrect Input")
-
+    print("Incorrect Input")
